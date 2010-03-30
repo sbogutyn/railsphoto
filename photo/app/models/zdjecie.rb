f9aabc53d4121ef34f0b48a33ba606b84d7889aa
slavo <slavo@slavo.(none)>
2010-03-30 23:01:22 +0200

    zamiana tabeli na divy

--- photo/app/models/zdjecie.rb
+++ photo/app/models/zdjecie.rb
@@ -1,8 +1,8 @@
 class Zdjecie < ActiveRecord::Base
     attr_accessible :opis, :photo
     has_attached_file :photo, :styles => {
-        :thumb=> "100x100#",
-        :small  => "150x150>",
+        :thumb=> "200x200#",
+        :small  => "300x300>",
     }  
 
 end
